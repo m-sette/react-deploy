db437c9f8e6e8a23d581476599206685103a51e6
Matteo <msette8@gmail.com>
2023-01-25 23:45:48 +0100

    Modify Home page again

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <h1>Hello from deploy</h1>
+            <h1>Hello from again</h1>
         </div>
     );
 }
